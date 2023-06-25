7991e883f3139bfe4b45af64ffa78a731c7eb373
AKVBI <adityaiyer52@gmail.com>
2023-06-25 11:53:37 +0530

    first commit

--- src/test/Hello_prg.java
+++ src/test/Hello_prg.java
@@ -4,8 +4,12 @@ public class Hello_prg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+int i=0;
 
+if(i==1)
 		System.out.println("Hello World");
+else
+	System.out.println("Welocme home");
 	}
 
 }
